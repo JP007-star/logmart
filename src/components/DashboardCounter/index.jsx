0adbcd61a2ed17bc4915040f0419d93b4ce7b53e
jaya prasad <justice_of_peace@yahoo.com>
2024-08-15 16:03:52 +0530

    css changes

--- src/components/DashboardCounter/index.jsx
+++ src/components/DashboardCounter/index.jsx
@@ -3,34 +3,32 @@ import * as Unicons from "@iconscout/react-unicons";
 import './style.css'; // Import the updated CSS file
 
 export const DashboardCounter = ({ initialData }) => {
-  console.log(initialData);
-  
   return (
-    <div className="dashboard-counter-container row ">
-      <div className="card-dash text-white bg-primary shadow-sm rounded" style={{ maxWidth: "14rem" }}>
+    <div className="dashboard-counter-container row">
+      <a href="/admin/products" className="card-dash text-white bg-primary shadow-sm rounded">
         <div className="card-header h4 text-center">
-        <Unicons.UilUtensils /> Products
+          <Unicons.UilUtensils /> Products
         </div>
         <div className="card-body h3 text-center">{initialData.products.length}</div>
-      </div>
-      <div href="/admin/orders"  className="card-dash text-white bg-warning shadow-sm rounded" style={{ maxWidth: "14rem" }}>
+      </a>
+      <a href="/admin/orders" className="card-dash text-white bg-warning shadow-sm rounded">
         <div className="card-header h4 text-center">
           <Unicons.UilTag /> Orders
         </div>
         <div className="card-body h3 text-center">{initialData.orders.length}</div>
-      </div>
-      <div className="card-dash text-white bg-danger shadow-sm rounded" style={{ maxWidth: "14rem" }}>
+      </a>
+      <a href="/admin/users" className="card-dash text-white bg-danger shadow-sm rounded">
         <div className="card-header h4 text-center">
-        <Unicons.UilUser /> Users
+          <Unicons.UilUser /> Users
         </div>
         <div className="card-body h3 text-center">{initialData.users.length}</div>
-      </div>
-      <div className="card-dash text-white bg-success shadow-sm rounded" style={{ maxWidth: "14rem" }}>
+      </a>
+      <a href="/admin/revenue" className="card-dash text-white bg-success shadow-sm rounded">
         <div className="card-header h4 text-center">
           <Unicons.UilMoneyBill /> Revenue
         </div>
         <div className="card-body h3 text-center">₹10,000</div>
-      </div>
+      </a>
     </div>
   );
 };
